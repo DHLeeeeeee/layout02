b4d7af36cf7485cafcd836cce95efe4f6841d18b
DH <97073004+YeeDubu@users.noreply.github.com>
2022-12-06 17:50:38 +0900

    메인 슬라이드, 배너 작업

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+  $('.mainSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    arrows: true,
+    dots: true,
+    draggable: false,
+  });
+
+  $('.slick-dots').wrap("<div class='slick-dots-container'></div>");
+  $('.slick-dots-container').append('<button class="btn"></button>');
+
+  var sw = 0;
+  $('.btn').click(function () {
+    if (sw == 0) {
+      $('.btn').addClass('on');
+      $('.mainSlide').slick('slickPause');
+      sw = 1;
+    } else {
+      $('.btn').removeClass('on');
+      $('.mainSlide').slick('slickPlay');
+      sw = 0;
+    }
+  });
+
+  $('.eventSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    arrows: true,
+    dots: true,
+    draggable: false,
+  });
+});
